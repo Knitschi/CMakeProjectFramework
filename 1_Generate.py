71829e51fd614e029870af33a3e7f0305341d195
Knitschi <knitschi@freenet.de>
2017-11-27 09:43:06 +0100

    Init project

---
+++ 1_Generate.py
@@ -0,0 +1,30 @@
+﻿#!/usr/bin/python3
+"""Usage:
+    1_Generate.py [<config_name>] [--help]
+
+    Running this script will run CMake to generate the "make-files" for the given configuration. 
+    <config_name> must be the base-name of a configuration file found in the "Configuration" sub-directory.
+    After calling this script, the makefiles can be found in the "Generated/<config-name> sub-directory.
+
+    When the <config_name> is given, an possibly existing "Generated/<config-name>" directory will be
+    deleted before running cmake. If the <config_name> option is not given, the script will choose
+    the first available config in the "Configuration" directory. It will also not delete the 
+    "Generated/<config-name>" directory, so this can be used for running CMake incrementally.
+
+    Note that when calling this for the first time, this step will download and compile all dependencies
+    that are handled with the hunter package manager, so the execution may take some time.
+
+Options:
+    -h --help               Show this
+
+"""
+import sys
+from Sources.CppCodeBaseBuildscripts.cppcodebasebuildscripts.docopt import docopt
+from Sources.CppCodeBaseBuildscripts.cppcodebasebuildscripts import buildautomat
+
+if __name__ == "__main__":
+    args = docopt(__doc__, version='1_Generate 1.0')
+    automat = buildautomat.BuildAutomat()
+    if not automat.generateMakefiles(args):
+        print("Error: Script 1_Generate.py failed.")
+        sys.exit(2)
---
+++ 2_Make.py
@@ -0,0 +1,30 @@
+﻿#!/usr/bin/python3
+"""Usage:
+    2_Make.py [<config_name>] [--target <target>] [--config <config>] [--help]
+
+    This script builds the given target in the given configuration.
+
+    Explicitly stating <config_name> will also cause a full rebuild if the target has previously been make.
+    If no <config_name> is given, the first configuration in the "Configuration" subdirectory will be used.
+    This configuration will then be make incrementally.
+
+    If no <target> is given, the "ALL_BUILD" target will be make.
+
+Options:
+    -h --help               Show this
+    --target <target>       Specify the build target.
+    --config <config>       Specify the configuration for multi-config build systems.
+                            This is usually Debug or Release. 
+
+""" 
+
+import sys
+from Sources.CppCodeBaseBuildscripts.cppcodebasebuildscripts.docopt import docopt
+import Sources.CppCodeBaseBuildscripts.cppcodebasebuildscripts.buildautomat as buildautomat
+
+if __name__ == "__main__":
+    args = docopt(__doc__, version='2_Make 1.0')
+    automat = buildautomat.BuildAutomat()
+    if not automat.make(args):
+        print("Error: Script 2_Make.py failed.")
+        sys.exit(2)
