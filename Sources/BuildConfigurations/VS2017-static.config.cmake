c7fcd5f2d17320a172d1e14b0f3cb3063ba01a47
Knitschi <knitschi@freenet.de>
2018-02-17 15:50:22 +0100

    Add CIBuildConfigurations as submodule.

--- Sources/BuildConfigurations/VS2017-static.config.cmake
+++
@@ -1,7 +0,0 @@
-set(CPF_CONFIG "VS2017-static" CACHE STRING "the config name" FORCE)
-set(CPF_CONFIG_FILE "${CMAKE_CURRENT_LIST_FILE}" CACHE FILEPATH "The path to the used .config.cmake file.")
-
-include("${CMAKE_CURRENT_LIST_DIR}/../CPFCMake/DefaultConfigurations/Windows.config.cmake")
-
-# LOCATIONS
-set( CPF_WEBPAGE_URL "http://buildmasterdebian9" CACHE STRING "An url from which the distribution of the last build can be downloaded." FORCE)
