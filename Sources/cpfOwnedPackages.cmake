0eb26c6ac461c4d891f8470cf6b4630d011d401d
Knitschi <knitschi@freenet.de>
2018-11-25 13:33:40 +0100

    - Update packages - Add footer and header to the CPFDocumentation pacakge.

--- Sources/cpfOwnedPackages.cmake
+++
@@ -1,9 +0,0 @@
-set( CPF_OWNED_PACKAGES
-"\
-CPFCMake;\
-CPFBuildscripts;\
-CPFMachines;\
-CPFTests;\
-"
-CACHE STRING "The packages that are owned by this CI-project." FORCE
-)
---
+++ Sources/cpfPackages.cmake
@@ -0,0 +1,10 @@
+set( CPF_PACKAGES
+    EXTERNAL cotire
+    OWNED CIBuildConfigurations
+    OWNED CPFDoxygenConfig
+    OWNED CPFCMake
+    OWNED CPFBuildscripts
+    OWNED CPFMachines
+    OWNED CPFTests
+    OWNED CPFDocumentation
+)
